a4c2e8fb659a37a3be771f6f8f68d88dffc95db2
SenseiZarkoDukic <zarko.dukic@gmail.com>
2024-07-01 16:32:20 +0200

    stylize flex and text of LittleLemonHeader

--- components/LittleLemonHeader.tsx
+++ components/LittleLemonHeader.tsx
@@ -3,16 +3,24 @@ import { View, Text } from "react-native";
 
 export function LittleLemonHeader() {
   return (
-    <View style={{ flex: 0.175, backgroundColor: "#F4CE14" }}>
+    <View style={{ flex: 0.2, backgroundColor: "#F4CE14" }}>
       <Text
         style={{
-          padding: 40,
+          padding: 30,
           fontSize: 30,
           color: "black",
           textAlign: "center",
         }}
+        numberOfLines={3}
       >
-        Little Lemon
+        Welcome to
+        <Text
+          style={{ fontWeight: "bold", color: "#495E57" }}
+          // numberOfLines={1}
+        >
+          {" "}
+          Little Lemon
+        </Text>
       </Text>
     </View>
   );
