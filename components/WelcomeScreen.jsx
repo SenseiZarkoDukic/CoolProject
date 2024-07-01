a3b88621d274f73352c3b8da96ecf984bf44b4e7
SenseiZarkoDukic <zarko.dukic@gmail.com>
2024-07-01 16:52:51 +0200

    style: Update WelcomeScreen component styles for better readability

--- components/WelcomeScreen.jsx
+++ components/WelcomeScreen.jsx
@@ -3,8 +3,10 @@ import { View, Text } from "react-native";
 export function WelcomeScreen() {
   return (
     <View>
-      <Text style={{ color: "white" }}>Welcome to the app!</Text>
-      <Text style={{ color: "white" }}>
+      <Text style={{ color: "white", textAlign: "center", fontSize: "24" }}>
+        Welcome to the app!
+      </Text>
+      <Text style={{ color: "white", textAlign: "center" }}>
         Little Lemon is a charming neighborhood bistro that serves simple food
         and classic cocktails in a lively but casual environment. We would love
         to hear more about your experience with us!
