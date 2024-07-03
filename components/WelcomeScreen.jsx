1c5b3da0a67147d50a75a3ec0ee3fbb2e0296ff0
SenseiZarkoDukic <zarko.dukic@gmail.com>
2024-07-03 18:00:11 +0200

    style: Refactor WelcomeScreen component to use StyleSheet for consistent styling and improve readability

--- components/WelcomeScreen.jsx
+++ components/WelcomeScreen.jsx
@@ -1,27 +1,10 @@
-import { View, Text, ScrollView } from "react-native";
+import { View, Text, ScrollView, StyleSheet } from "react-native";
 
 export function WelcomeScreen() {
   return (
-    <ScrollView indicatorStyle={"white"} style={{ flex: 1 }}>
-      <Text
-        style={{
-          color: "#EDEFEE",
-          textAlign: "center",
-          fontSize: 50,
-          padding: 40,
-        }}
-      >
-        Welcome to Little Lemon
-      </Text>
-      <Text
-        style={{
-          color: "#EDEFEE",
-          textAlign: "center",
-          marginVertical: 8,
-          fontSize: 38,
-          padding: 20,
-        }}
-      >
+    <ScrollView indicatorStyle={"white"} style={welcomeStyles.container}>
+      <Text style={welcomeStyles.whiteHeaderText}>Welcome to Little Lemon</Text>
+      <Text style={welcomeStyles.whiteText}>
         Little Lemon is a charming neighborhood bistro that serves simple food
         and classic cocktails in a lively but casual environment. We would love
         to hear more about your experience with us!
@@ -29,3 +12,21 @@ export function WelcomeScreen() {
     </ScrollView>
   );
 }
+
+const welcomeStyles = StyleSheet.create({
+  container: { flex: 1 },
+
+  whiteHeaderText: {
+    color: "#EDEFEE",
+    textAlign: "center",
+    fontSize: 50,
+    padding: 40,
+  },
+  whiteText: {
+    color: "#EDEFEE",
+    textAlign: "center",
+    marginVertical: 8,
+    fontSize: 38,
+    padding: 20,
+  },
+});
