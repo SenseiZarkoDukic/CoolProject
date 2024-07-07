3308e81b6d630d72407691f2379a3afead6e25ea
SenseiZarkoDukic <zarko.dukic@gmail.com>
2024-07-07 03:41:40 +0200

    Refactor MenuItems component to include categorized data, separate name and price in item text, and improve performance and readability

--- components/MenuItems.jsx
+++ components/MenuItems.jsx
@@ -36,37 +36,81 @@ const yellow = "#F4CE14";
 //   { name: "Panna Cotta", price: "$5.00", id: "21V" },
 // ];
 
+// const menuItemsToDisplay = [
+//   {
+//     title: "Appetizers",
+//     data: [
+//       "Hummus",
+//       "Moutabal",
+//       "Falafel",
+//       "Marinated Olives",
+//       "Kofta",
+//       "Eggplant Salad",
+//     ],
+//   },
+//   {
+//     title: "Main Dishes",
+//     data: ["Lentil Burger", "Smoked Salmon", "Kofta Burger", "Turkish Kebab"],
+//   },
+//   {
+//     title: "Sides",
+//     data: [
+//       "Fries",
+//       "Buttered Rice",
+//       "Bread Sticks",
+//       "Pita Pocket",
+//       "Lentil Soup",
+//       "Greek Salad",
+//       "Rice Pilaf",
+//     ],
+//   },
+//   {
+//     title: "Desserts",
+//     data: ["Baklava", "Tartufo", "Tiramisu", "Panna Cotta"],
+//   },
+// ];
+
 const menuItemsToDisplay = [
   {
     title: "Appetizers",
     data: [
-      "Hummus",
-      "Moutabal",
-      "Falafel",
-      "Marinated Olives",
-      "Kofta",
-      "Eggplant Salad",
+      { name: "Hummus", price: "$5.00" },
+      { name: "Moutabal", price: "$5.00" },
+      { name: "Falafel", price: "$7.50" },
+      { name: "Marinated Olives", price: "$5.00" },
+      { name: "Kofta", price: "$5.00" },
+      { name: "Eggplant Salad", price: "$8.50" },
     ],
   },
   {
     title: "Main Dishes",
-    data: ["Lentil Burger", "Smoked Salmon", "Kofta Burger", "Turkish Kebab"],
+    data: [
+      { name: "Lentil Burger", price: "$10.00" },
+      { name: "Smoked Salmon", price: "$14.00" },
+      { name: "Kofta Burger", price: "$11.00" },
+      { name: "Turkish Kebab", price: "$15.50" },
+    ],
   },
   {
     title: "Sides",
     data: [
-      "Fries",
-      "Buttered Rice",
-      "Bread Sticks",
-      "Pita Pocket",
-      "Lentil Soup",
-      "Greek Salad",
-      "Rice Pilaf",
+      { name: "Fries", price: "$3.00", id: "11K" },
+      { name: "Buttered Rice", price: "$3.00" },
+      { name: "Bread Sticks", price: "$3.00" },
+      { name: "Pita Pocket", price: "$3.00" },
+      { name: "Lentil Soup", price: "$3.75" },
+      { name: "Greek Salad", price: "$6.00" },
+      { name: "Rice Pilaf", price: "$4.00" },
     ],
   },
   {
     title: "Desserts",
-    data: ["Baklava", "Tartufo", "Tiramisu", "Panna Cotta"],
+    data: [
+      { name: "Baklava", price: "$3.00" },
+      { name: "Tartufo", price: "$3.00" },
+      { name: "Tiramisu", price: "$5.00" },
+      { name: "Panna Cotta", price: "$5.00" },
+    ],
   },
 ];
 
@@ -85,7 +129,7 @@ const Item = ({ name, price }) => (
     <Text style={menuStyles.itemTextName} numberOfLines={1}>
       {name}
     </Text>
-    {/* <Text style={menuStyles.itemTextPrice}>{price}</Text> */}
+    <Text style={menuStyles.itemTextPrice}>{price}</Text>
   </View>
 );
 
@@ -94,7 +138,9 @@ const MenuItems = () => {
   //   <Item name={item.name} price={item.price} id={item.id} />
   // );
 
-  const renderItem = ({ item }) => <Item name={item} />;
+  const renderItem = ({ item: { name, price } }) => (
+    <Item name={name} price={price} />
+  );
 
   const renderSectionHeader = ({ section: { title } }) => (
     <Text style={menuStyles.sectionHeaderTitle}>{title}</Text>
