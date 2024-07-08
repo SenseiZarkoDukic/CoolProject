0218ac653bb5a3aa74ac1aa79fd5b5539af095fa
SenseiZarkoDukic <zarko.dukic@gmail.com>
2024-07-08 16:26:24 +0200

    Refactor FeedbackForm component to update heading and info sections

--- components/FeedbackForm.jsx
+++ components/FeedbackForm.jsx
@@ -9,7 +9,14 @@ export function FeedbackForm() {
 
   return (
     <ScrollView style={feedbackStyles.container}>
-      <Text style={feedbackStyles.headerText}>Feedback Form</Text>
+      <Text style={feedbackStyles.headingSection}>
+        How was your visit to Little Lemon?
+      </Text>
+      <Text style={feedbackStyles.infoSection}>
+        Little Lemon is a charming neighborhood bistro that serves simple food
+        and classic cocktails in a lively but casual environment. We would love
+        to hear your experience with us!
+      </Text>
       <TextInput
         style={feedbackStyles.input}
         onChangeText={onChangeFirstName}
@@ -34,19 +41,39 @@ export function FeedbackForm() {
 }
 
 const feedbackStyles = StyleSheet.create({
-  container: { flex: 1 },
-  headerText: {
-    color: "#EDEFEE",
-    textAlign: "center",
-    fontSize: 50,
-    padding: 40,
-  },
+  container: { flex: 1, backgroundColor: "#495E57" },
   input: {
     height: 40,
     margin: 12,
     borderWidth: 1,
     padding: 10,
+    fontSize: 16,
+    borderColor: "EDEFEE",
     backgroundColor: "#F4CE14",
+  },
+  messageInput: {
+    height: 100,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
+    fontSize: 16,
+    backgroundColor: "#F4CE14",
+  },
+  infoSection: {
+    fontSize: 24,
+    padding: 20,
+    marginVertical: 8,
+    color: "#EDEFEE",
+    textAlign: "center",
+    backgroundColor: "#495E57",
+  },
+  headingSection: {
+    fontSize: 28,
+    padding: 20,
+    marginVertical: 8,
+    color: "#EDEFEE",
+    textAlign: "center",
+    backgroundColor: "#495E57",
   },
 });
 
